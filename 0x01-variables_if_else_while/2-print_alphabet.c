7ad22676d9e757b27b00cc5c8055956e553ce738
StanleyGemstone <stanleynduchi@gmail.com>
2022-12-09 16:33:59 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char aph;
+
+	for (aph = "a"; aph <= "z"; aph++)
+	{
+		putchar(aph)
+	}
+	putchar("\n");
+	return (0);
+}
