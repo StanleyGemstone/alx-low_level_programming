8b1b2a9e10fe9cfef72168e1bfd350ed9982df11
StanleyGemstone <stanleynduchi@gmail.com>
2022-12-11 10:23:52 -0800

    2-print_alphabet.c gotten too

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = "a"; ch <= "z"; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch = "A"; ch <= "z"; ch++)
+	for (ch = 'A'; ch <= 'z'; ch++)
 		putchar(ch);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
