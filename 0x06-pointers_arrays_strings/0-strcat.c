0cff3d9a4a62b7347dcd8c1fb00d4972f99981d8
StanleyGemstone <stanleynduchi@gmail.com>
2022-12-22 23:01:54 +0100

    poppo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,20 +6,20 @@
  * @src: copy from
  * Return: pointer of dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
 	i = 0;
+
 	while(dest[i] != '\0')
 	{
 		i++;
 	}
 	while(src[j] != '\0')
 	{
-		dest[i] = src[j];
+		dest[i] + src[j];
 		i++;
 		j++;
 	}
