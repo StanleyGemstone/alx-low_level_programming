347e731fda7faa4edf153a0646868384565fdbcb
StanleyGemstone <stanleynduchi@gmail.com>
2022-12-22 23:50:45 +0100

    Advance task2

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,6 +10,7 @@ char *rot13(char *s)
 	int i, j;
 	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; x[j] != '\0'; j++)
