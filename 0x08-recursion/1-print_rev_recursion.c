81055939c4fa704cc066ffdb6db9d1ccccb424f4
StanleyGemstone <stanleynduchi@gmail.com>
2023-01-05 01:02:43 +0100

    All 0x08 Project

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints string in rev
+ * @s: string to print
+ * Return: Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+	
+	if (s[i] == '\0')
+		return;
+	else
+		_print_rev_recursion(&s[i + 1]);
+	_putchar(*s);
+}
