aba32b5a254953665f9db96023c308d58249f92c
StanleyGemstone <stanleynduchi@gmail.com>
2023-01-09 18:03:50 +0100

    Task 101 again

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -55,7 +55,17 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
+		else if (c++ == 0)
+			start = i;
 	}
+	matrix[k] = NULL;
+	return (matrix);
 }
