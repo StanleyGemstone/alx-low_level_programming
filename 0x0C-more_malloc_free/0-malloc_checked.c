02529d56a42efc64d068149869fd95453931d770
StanleyGemstone <stanleynduchi@gmail.com>
2023-01-12 21:09:18 +0100

    mended error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,14 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocates
+ *
  * Return: a pointer to the allocated memory.
  */
-
 void *malloc_checked(unsigned int b)
 {
 	char *p;
 
 	p = malloc(b);
-
 	if (p == NULL)
 		exit(98);
 	return (p);
